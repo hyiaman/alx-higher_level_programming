1bd577ba42e9958958fe9ae96b0d3a2d03f50d4d
hyiaman <hyiamosei@gmail.com>
2024-08-11 00:13:44 -0700

    5 to integer error fixed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,4 +3,4 @@
 const { argv } = require('process');
 const num = parseInt(argv[2]);
 
-console.log(Number.isInteger(num) ? 'My number: ${num}' : 'Not a number');
+console.log(Number.isInteger(num) ? `My number: ${num}` : 'Not a number');
